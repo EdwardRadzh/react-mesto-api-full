ef15a944eb094a0da82d54c7b715b296445502ec
EdwardRadzh <69647887+EdwardRadzh@users.noreply.github.com>
2021-10-08 23:10:10 +0300

    bugfix

--- backend/app.js
+++ backend/app.js
@@ -18,12 +18,13 @@ const NotFound = require('./errors/NotFound');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(cors());
+
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
 
 app.use(helmet());
 app.use(requestLogger);
