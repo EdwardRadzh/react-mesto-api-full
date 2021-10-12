427d69ed12e27682c143061b58b77d47c5bb584b
EdwardRadzh <69647887+EdwardRadzh@users.noreply.github.com>
2021-10-12 12:52:02 +0300

    bugFix

--- backend/app.js
+++ backend/app.js
@@ -41,7 +41,6 @@ app.use((req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
